e025838d0e984e65e11e0e56d63cb02f155009a1
Erich Abbott <abbotterich@gmail.com>
2015-11-10 15:27:30 -0600

    fb broken code

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_DB_CONN_OUSTED);
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
